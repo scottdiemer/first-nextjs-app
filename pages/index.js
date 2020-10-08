8cab32f3c42e6da9da6d4ab91ff859a8cd0cacd7
scottdiemer <sdiemer@cfl.rr.com>
2020-10-07 22:36:54 -0400

    build navbar and started hero

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       {/* header */}
-      <div className="bg-green-700 p-4 flex justify-between items-center">
+      <div className="flex flex-col md:flex-row bg-green-900 p-4 flex justify-between items-center">
         {/* left side */}
-        <div className="flex items-center">
+        <div className="flex items-start md:items-center">
           <img
             width="50"
             src="https://cdn1.iconfinder.com/data/icons/casino-solid-royal-straight-flush/512/Horseshoes-512.png"
@@ -41,6 +40,36 @@ export default function Home() {
         </div>
       </div>
       {/* hero */}
+      <div className="md:flex justify-between py-20 px-10 bg-green-100 text-green-900">
+        {/* left */}
+        <div className="md:w-1/2 mb-10 md:mb-0">
+          <h2 className="text-2xl md:text-4xl lg:text-6xl text-black mb-6">
+            Welcome to Farm City
+          </h2>
+          <p className="mb-6">Keep them at their peek performance!</p>
+
+          <a
+            href="#"
+            className="inline-block py-3 px-6 text-lg bg-gray-300 text-gray-800 rounded-lg mr-2"
+          >
+            Learn More
+          </a>
+          <a
+            href="#"
+            className="inline-block py-3 px-6 text-lg bg-orange-500 rounded-lg text-gray-100 hover:bg-orange-400 hover:text-pink-700"
+          >
+            Get Started
+          </a>
+        </div>
+        {/* right */}
+        <div className="md:w-1/2">
+          <img
+            src="https://images.unsplash.com/photo-1600792178703-43239f15b6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+            alt="Peak Performance"
+            className="w-full"
+          />
+        </div>
+      </div>
       {/* features */}
       {/* footer + newsletter */}
     </>
