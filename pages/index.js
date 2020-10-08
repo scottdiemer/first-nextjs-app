5fddcb57f10c432b4519dc15b11d078dbc6f9175
scottdiemer <sdiemer@cfl.rr.com>
2020-10-07 20:25:41 -0400

    configured tailwindcss and built a navbar

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+    <>
+      {/* header */}
+      <div className="bg-green-700 p-4 flex justify-between items-center">
+        {/* left side */}
+        <div className="flex items-center">
+          <img
+            width="50"
+            src="https://cdn1.iconfinder.com/data/icons/casino-solid-royal-straight-flush/512/Horseshoes-512.png"
+            alt="logo"
+            className="mr-2"
+          />
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            href="#"
+            className="inline-block p-3 text-green-200 hover:text-green-100"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            Home
           </a>
-
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            href="#"
+            className="inline-block p-3 text-green-200 hover:text-green-100"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            About
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        {/* right side */}
+        <div>
+          <a href="#" className="inline-block p-2 text-green-200 mr-2">
+            Login
+          </a>
+          <a
+            href="#"
+            className="inline-block py-2 px-4 text-green-700 bg-yellow-500 rounded hover:bg-yellow-400 hover:text-green-800 transition ease-in duration-150"
+          >
+            Sign up
+          </a>
+        </div>
+      </div>
+      {/* hero */}
+      {/* features */}
+      {/* footer + newsletter */}
+    </>
+  );
 }
