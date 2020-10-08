dd9ea965dbdf7df0ffac0c28745159d0183a6dc1
scottdiemer <sdiemer@cfl.rr.com>
2020-10-08 17:01:18 -0400

    built image feature and footer

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,60 @@ export default function Home() {
         </div>
       </div>
       {/* features */}
+      <div className="md:flex py-16 px-10 bg-green-700 text-green-200 text-center">
+        <div className="mr-2 text-center">
+          <img
+            src="https://images.unsplash.com/photo-1553284965-e2815db2e5a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            alt=""
+            className="w-full mb-4 rounded border-solid border-2 border-green-200"
+          />
+          <p>Horses</p>
+        </div>
+        <div className="mr-2 text-center mt-8">
+          <img
+            src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            alt=""
+            className="w-full mb-4 rounded border-solid border-2 border-green-200"
+          />
+          <p>Dogs</p>
+        </div>
+        <div className="mr-2 text-center">
+          <img
+            src="https://images.unsplash.com/photo-1527416876370-fb74d128c3dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            alt=""
+            className="w-full mb-4 rounded border-solid border-2 border-green-200"
+          />
+          <p>Cats</p>
+        </div>
+        <div className="mr-2 text-center mt-8">
+          <img
+            src="https://images.unsplash.com/photo-1597999966821-50aa60e5c7be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+            alt=""
+            className="w-full mb-4 rounded border-solid border-2 border-green-200"
+          />
+          <p>Chickens</p>
+        </div>
+      </div>
       {/* footer + newsletter */}
+      <div className="p-10 bg-green-900 text-green-400 flex justify-between">
+        {/* left */}
+        <div className="md:w-1/2">
+          <h3 className="text-lg mb-2">Join the Newsletter!</h3>
+          <form className="flex">
+            <input
+              type="email"
+              className="w-full rounded-l py-3 px-4 outline-none text-green-900 focus:bg-green-100"
+            />
+            <button className="bg-orange-500 hover:bg-orange-700 text-orange-900 hover:text-orange-400 rounded-r px-4">
+              Join
+            </button>
+          </form>
+        </div>
+        {/* right */}
+        <div className="flex items-center">
+          Copyright &copy; Farm City Inc 2020
+        </div>
+      </div>
     </>
   );
 }
