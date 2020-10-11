b423e8e10e3a949c5a78bd0d8be823bbf79a5476
scottdiemer <sdiemer@cfl.rr.com>
2020-10-11 12:18:33 -0400

    broke card component down into multiple cards using composition for reuseablity

--- components/card.js
+++ components/card.js
@@ -1,23 +1,49 @@
-const Card = ({ image, title, description, price }) => {
+export const CardLayout = ({ children }) => {
+  return (
+    <article className="p-4 bg-white border rounded-lg shadow-lg">
+      {children}
+    </article>
+  );
+};
+
+export const Card = ({ title, description, children }) => {
   return (
     <>
-      <article className="p-4 bg-white border rounded-lg shadow-lg">
-        <img className="w-24 h-auto mx-auto" src={image} alt="" />
-        <div className="p-4">
-          <h3 className="mt-0 mb-4 font-bold sm:text-lg md:text-xl lg:text-2xl">
-            {title}
-          </h3>
-          <p className="text-sm text-gray-700">{description}</p>
-          <h4 className="mt-4 font-bold text-right text-gray-500 line-through">
-            Price: ${price}
-          </h4>
-          <h4 className="mt-4 font-bold text-right text-green-900">
-            Now: ${(price * 0.8).toFixed(2)}
-          </h4>
-        </div>
-      </article>
+      <div className="p-4">
+        <h3 className="mt-0 mb-4 font-bold sm:text-lg md:text-xl lg:text-2xl">
+          {title}
+        </h3>
+        <p className="text-sm text-gray-700">{description}</p>
+      </div>
+      {children}
     </>
   );
 };
 
-export default Card;
+export const ImageCard = ({ image, title, description, children }) => {
+  return (
+    <>
+      <img className="w-24 h-auto mx-auto" src={image} alt="" />
+      <Card title={title} description={description}>
+        {children}
+      </Card>
+    </>
+  );
+};
+
+const SaleCard = ({ image, title, description, price }) => {
+  return (
+    <>
+      <ImageCard image={image} title={title} description={description}>
+        <h4 className="mt-4 font-bold text-right text-gray-500 line-through">
+          Price: ${price.toFixed(2)}
+        </h4>
+        <h4 className="mt-4 font-bold text-right text-green-900">
+          Now: ${(price * 0.8).toFixed(2)}
+        </h4>
+      </ImageCard>
+    </>
+  );
+};
+
+export default SaleCard;
--- components/specials.js
+++ components/specials.js
@@ -1,38 +1,39 @@
 import { useEffect, useState } from "react";
-import Card from "./card";
+import SaleCard, { CardLayout } from "./card";
 
-const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+const Specials = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState();
 
-  useEffect(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    // const [items] = data.results;
-    // setData(items);
-    // setLoading(false);
+  useEffect(() => {
+    setLoading(true);
+    async function getProducts() {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products/?limit=10"
+        );
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getProducts();
   }, []);
 
-  return { data, loading };
-};
-const Specials = () => {
-  const { data: products, loading } = useFetch(
-    "https://fakestoreapi.com/products/?limit=5"
-  );
-
   return (
-    <>
-      <section className="p-20 bg-green-800 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {loading ? (
-          <div>loading...</div>
-        ) : (
-          products.map((product) => {
-            <Card key={product.id} product={product} />;
-          })
-        )}
-      </section>
-    </>
+    <section className="p-20 bg-green-800 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      {loading ? (
+        <p className="font-bold text-green-200">Loading...</p>
+      ) : (
+        products.map((product, index) => (
+          <CardLayout>
+            <SaleCard key={index} {...product} />
+          </CardLayout>
+        ))
+      )}
+    </section>
   );
 };
 
